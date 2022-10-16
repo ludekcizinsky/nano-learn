5407f8e86d2e8b9d75213ae28265dbf975fd8a6f
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-10-17 01:11:55 +0200

    Exceptions: added not FeatureNotImplemented

--- nnlearn/exceptions/__init__.py
+++ nnlearn/exceptions/__init__.py
@@ -2,9 +2,10 @@
 The `nnlearn.exceptions` module provides custom defined errors which can occur during execution.
 """
 
-from ._base import DimensionMismatchError, CriterionFunctionNotFound
+from ._base import DimensionMismatchError, CriterionFunctionNotFound, FeatureNotImplemented
 
 __all__ = [
     'DimensionMismatchError',
-    'CriterionFunctionNotFound'
+    'CriterionFunctionNotFound',
+    'FeatureNotImplemented'
 ]
--- nnlearn/exceptions/_base.py
+++ nnlearn/exceptions/_base.py
@@ -35,3 +35,19 @@ class CriterionFunctionNotFound(Error):
     
     def __repr__(self):
         return self.message
+
+class FeatureNotImplemented(Error):
+    """
+    Given feature has not been implemented.
+
+    Parameters
+    ----------
+    message : str
+        Explanation of what happened.
+    """
+
+    def __init__(self, message):
+        self.message = message
+    
+    def __repr__(self):
+        return self.message
