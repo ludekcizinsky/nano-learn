7ac4057a5d728b3b62cd6c7b898e189a55ee0b02
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-10-17 01:14:47 +0200

    Nanograd: created specific folder for nanograd and defined two new methods - exp, log

---
+++ nnlearn/nanograd/__init__.py
@@ -0,0 +1,11 @@
+"""
+The module ``nnlearn.nanograd`` includes implementation
+of a variable which enables this micro-framework to find 
+optimal parameters for parametric models.
+"""
+
+from ._nanograd import Var
+
+__all__ = [
+    'Var'
+]
---
+++ nnlearn/nanograd/_nanograd.py
@@ -0,0 +1,64 @@
+from typing import Union
+from math import tanh
+import numpy as np
+
+class Var:
+    """
+    A variable which holds a number and enables gradient computations.
+
+    Notes
+    -----
+    The author of this class is: Rasmus Berg Palm
+    Github repo: https://github.com/rasmusbergpalm/nanograd/blob/main/nanograd.py
+    """
+
+    def __init__(self, val: Union[float, int], parents=None):
+
+        if parents is None:
+            parents = []
+        self.v = val
+        self.parents = parents
+        self.grad = 0.0
+
+    def backprop(self, bp):
+        self.grad += bp
+        for parent, grad in self.parents:
+            parent.backprop(grad * bp)
+
+    def backward(self):
+        self.backprop(1.0)
+
+    def __add__(self: 'Var', other: 'Var') -> 'Var':
+        return Var(self.v + other.v, [(self, 1.0), (other, 1.0)])
+
+    def __mul__(self: 'Var', other: 'Var') -> 'Var':
+        return Var(self.v * other.v, [(self, other.v), (other, self.v)])
+
+    def __pow__(self, power: Union[float, int]) -> 'Var':
+        assert type(power) in {float, int}, "power must be float or int"
+        return Var(self.v ** power, [(self, power * self.v ** (power - 1))])
+
+    def __neg__(self: 'Var') -> 'Var':
+        return Var(-1.0) * self
+
+    def __sub__(self: 'Var', other: 'Var') -> 'Var':
+        return self + (-other)
+
+    def __truediv__(self: 'Var', other: 'Var') -> 'Var':
+        return self * other ** -1
+
+    def tanh(self) -> 'Var':
+        return Var(tanh(self.v), [(self, 1 - tanh(self.v) ** 2)])
+
+    def relu(self) -> 'Var':
+        return Var(self.v if self.v > 0.0 else 0.0, [(self, 1.0 if self.v > 0.0 else 0.0)])
+
+    def log2(self) -> 'Var':
+        return Var(np.log2(self.v), [(self, 1 / self.v * np.log(2))]) 
+
+    def exp(self) -> 'Var':
+        return Var(np.exp(self.v), [(self, np.exp(self.v))])
+
+    def __repr__(self):
+        return "Var(v=%.4f, grad=%.4f)" % (self.v, self.grad)
+
