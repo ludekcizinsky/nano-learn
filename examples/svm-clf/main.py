0d22458b7d9df71a3f48636bc13cb76ec94a16c4
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-10-25 11:30:07 +0200

    SVM: completed the vectorization of the loss function

--- examples/svm-clf/main.py
+++ examples/svm-clf/main.py
@@ -1,29 +1,71 @@
 import os 
 import sys
-sys.path.insert(0, os.path.abspath(''))
+sys.path.insert(0, os.path.abspath('../..'))
 
 from nnlearn.util import ScriptInformation
 from nnlearn.svm import SVM
 
 import numpy as np
 
+
+def generate_data():
+
+    """Generate random dataset for classification purposes
+
+    Todo
+    ----
+    Move this to a separate module
+    """
+
+    # To make things repeatable
+    np.random.seed(1)
+    
+    # Set size of the sample
+    N = 20
+
+    # Generate features
+    # * sample two features from gaussian distribution 
+    X = np.array(list(zip(np.random.normal(size=N), np.random.normal(size=N))))
+
+    # * indices of points (x,y) for which XOR(x,y) is true - needed for y
+    c0 = np.where(X[:,0] < 0)
+    # * indices of data points belonging to the other class - needed for y
+    c1 = np.where(X[:,0] > 0)  
+    
+    # * apply rotation on the dataset
+    theta = np.radians(45)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array([[c,-s],[s,c]]) # rotation matrix
+    X =  X @ R
+
+    # Get targets
+    y = np.ones(N)  # labels
+    y[c1] = -1  # negative class labels
+    X[c1,0] = X[c1,0] + 0.1  # make a little gap between the two groups 
+
+    return X, y
+
 def test_svm_classifier():
     
+    # Get data
+    X, y = generate_data()
+
     # Initiliaze logger
     s = ScriptInformation()
+    
+    # Define kernel function (I chose simple dot product)
+    k = lambda x, y: np.dot(x, y)
 
     # Initialize model
-    clf = SVM()
-
+    clf = SVM(kernel=k)
+    
     # -- TODO: delete this in next commit
-    # -- Example showing how to use _gram method
-    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    k = lambda x, y: np.dot(x, y)
-    print()
-    print(a)
-    print()
-    print(clf._gram(a, k, "less_naive"))
-    # -- End of the section to be deletd
+    # Example showing how to use loss function
+    # * initial random parameters
+    a0 = np.random.rand(X.shape[0])
+    initial_loss = clf._loss(X, a0, y)
+    print(initial_loss)
+    # -- End of the section to be deleted
 
 if __name__ == '__main__':
     test_svm_classifier()
--- nnlearn/svm/_svm.py
+++ nnlearn/svm/_svm.py
@@ -3,16 +3,64 @@ from nnlearn.exceptions import FeatureNotImplemented
 import numpy as np
 
 class SVM:
-    def __init__(self):
-        pass
+    """Support vector machine model
+
+    Attributes
+    ----------
+    kernel : function
+             Function that takes two m-dimensional vectors as input
+             and returns single scalar value output
+    """
+    def __init__(self, kernel):
+        self.k = kernel
 
     def fit(self):
         pass
 
     def predict(self):
         pass
+
+    def _loss(self, X, a, y):
+        """Return loss
+
+        Attributes
+        ----------
+        X : 2D array
+            Design matrix, i.e., matrix where each row represents a sample
+
+        a : 1D array
+            One dimensional array of size N where N is the number of samples.
+            See notes for details.
+
+        y : 1D array
+            One dimensional array of size N representing true values
+            of our target variable.
+
+        Returns
+        -------
+        loss : float
+               Loss for the given set of parameters a.
+        
+        Notes
+        -----
+        TODO: information about the loss function
+        """
+
+        # Reshape a and y such that they fit the formula for A
+        a = a.reshape(1,-1)
+        yv = y.reshape(-1,1)
+
+        # Get A
+        K = self._gram(X)
+        A = (a.T @ a) * (yv @ yv.T) * K
+
+        # Compute the loss
+        loss = -(np.sum(a) - 1/2*np.sum(A))
+        
+        return loss
+
     
-    def _gram(self, X, k, how='less_naive'):
+    def _gram(self, X, how='less_naive'):
         
         """Compute the Gram matrix
         
@@ -26,10 +74,6 @@ class SVM:
         X : 2d array
             Design matrix where each row represents a sample
 
-        k : function
-            Function that takes two m-dimensional vectors as input
-            and returns single scalar value output
-
         how : string
             One of following (naive, less-naive) - see notes for
             more info.
@@ -42,26 +86,24 @@ class SVM:
         """
     
         if how == 'naive':
-            return self._gram_naive(X, k)
+            return self._gram_naive(X)
         elif how == 'less_naive':
-            return self._gram_less_naive(X, k)
+            return self._gram_less_naive(X)
         else:
             text = f"{how} has not been implemented, see docs for valid input."
             raise FeatureNotImplemented(text)
  
-    @staticmethod
-    def _gram_naive(X, k):
+    def _gram_naive(self, X):
 
         N = X.shape[0]
         K = np.zeros((N, N))
         for i in range(N):
             for j in range(N):
-                K[i, j] = k(X[i], X[j])
+                K[i, j] = self.k(X[i], X[j])
 
         return K
     
-    @staticmethod
-    def _gram_less_naive(X, k):
+    def _gram_less_naive(self, X):
 
         # Get all combinations of vectors
         n = X.shape[0]
@@ -72,7 +114,7 @@ class SVM:
         # Compute the value using the given kernel
         result = np.zeros((n, n))
         pairsnum = xi.shape[0]
-        result[i,j] = [k(xi[i], xj[i]) for i in range(pairsnum)]
+        result[i,j] = [self.k(xi[i], xj[i]) for i in range(pairsnum)]
         result = (result + result.T)
 
         # Adjust the main digonal to count it only once
